7fa0179700a2fc521d7b7829b15546334d76088f
ardra-ashok <ardraasok@yahoo.in>
2025-04-27 21:34:19 +1000

    java basic refreshing - operators

--- src/main/java/org/example/Operators.java
+++ src/main/java/org/example/Operators.java
@@ -62,5 +62,30 @@ public class Operators {
         int c = 4;
         System.out.println(~c);
 
+        System.out.println("-------------------");
+        System.out.println("Assignment Operator");
+
+        int a = 10;
+        System.out.println("Initial value of a: " + a);
+
+        a += 5;
+        System.out.println("After a += 5: " + a);  // 15
+
+        a -= 3;
+        System.out.println("After a -= 3: " + a);  // 12
+
+        a *= 2;
+        System.out.println("After a *= 2: " + a);  // 24
+
+        a /= 4;
+        System.out.println("After a /= 4: " + a);  // 6
+
+        a %= 5;
+        System.out.println("After a %= 5: " + a);  // 1
+
+
+
+
+
     }
 }
