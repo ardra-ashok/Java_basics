5240d6cb056acca141577113c93db5b112aeacf6
ardra-ashok <ardraasok@yahoo.in>
2025-04-30 22:37:50 +1000

    java basic refreshing - conditional statements

---
+++ src/main/java/org/example/ControlStatements.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ControlStatements {
+
+    /*. Conditional statements */
+    /*
+    * 1. if
+    * 2. if else
+    * 3. nested if else
+    * 4. switch case
+    * */
+
+}
